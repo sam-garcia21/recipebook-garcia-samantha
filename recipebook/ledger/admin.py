ce5c3f6db30ce8df28829c470f04c92463e66764
Sam Garcia <samantha.sofia.garcia@student.ateneo.edu>
2026-02-19 17:40:29 +0800

    created admin panels

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+from .models import Recipe, RecipeIngredient, Ingredient
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeIngredientInline]
+
+    search_fields = ('name',)
+
+    list_display = ('name',)
+
+    list_filter = ('name',)
+
+    fieldsets = [
+        ('Details', {
+            'fields': [
+                'name', 
+            ]
+        })
+    ]
+
+admin.site.register(Recipe, RecipeAdmin)
+
 # Register your models here.
