6aa058893eca53532e5e2188bc3020601d59c2f4
Sam Garcia <samantha.sofia.garcia@student.ateneo.edu>
2026-02-19 16:01:21 +0800

    created model classes

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def get_absolute_url(self):
+        return reverse({'recipe_detail', args[str(self.id)]})
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def get_absolute_url(self):
+        return reverse({'recipe_detail', args[str(self.id)]})
+
+class RecipeIngredient(models.Model):
+    quantity = models.IntegerField()
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE
+        related_name='recipe'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE
+        related_name='ingredients'
+    )
