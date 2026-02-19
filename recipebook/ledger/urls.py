d46193a5fce21dc62e012dca9dbab8f0c634bfc6
Sam Garcia <samantha.sofia.garcia@student.ateneo.edu>
2026-02-19 15:12:44 +0800

    updated app-level and project-level urls.py

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import recipe_list, recipe1, recipe2
 
 urlpatterns = [
-    path('list', recipe_list, name='recipe_list'),
-    path('1', recipe1, name='recipe1'),
-    path('2', recipe2, name='recipe2')
+    path('recipes/list', recipe_list, name='recipe_list'),
+    path('recipe/1', recipe1, name='recipe1'),
+    path('recipe/2', recipe2, name='recipe2')
 ]
 
 app_name = 'ledger'
